4b2d6be4114e43640bb6893997ce5eb48f21de9b
Raj026 <rajpatani31@gmail.com>
2022-04-14 14:36:04 +0530

    bottom navigation done with fragments

---
+++ app/src/main/java/com/example/capstone_project/Home_fragment.kt
@@ -0,0 +1,25 @@
+package com.example.capstone_project
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Home_fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_fragment, container, false)
+    }
+
+    companion object{
+        fun newinstance() = Home_fragment()
+    }
+
+
+}
